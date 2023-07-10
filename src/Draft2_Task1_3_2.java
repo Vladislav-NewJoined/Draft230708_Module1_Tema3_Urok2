bb87c14aedd2a2ac890ef803d8649c275c01085f
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-10 13:13:40 +0300

    Initial commit

--- src/Draft2_Task1_3_2.java
+++ src/Draft2_Task1_3_2.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Draft2_Task1_3_2 {
     public static void main(String[] args) {
-        System.out.println("Задание : \n2.\tНапишите метод, который возвращает наиболее из двух " +
+        System.out.println("Задание : \n2.\tНапишите метод, который возвращает наибольшее из двух " +
                 "дробных чисел\n\nРешение: ");
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Введите первое число: ");
-            int x = scanner.nextInt();
-            System.out.print("Введите второе число: ");
-            int y = scanner.nextInt();
+            System.out.print("Введите первое дробное число (с разделителем 'запятая'): ");
+            double x = scanner.nextDouble();
+            System.out.print("Введите второе дробное число (с разделителем 'запятая'): ");
+            double y = scanner.nextDouble();
 
-            int max = findMax(x, y);
+            double max = findMax(x, y);
 //            if (x>y) {
 //                max = x;
 //            } else {
@@ -18,11 +18,11 @@ public class Draft2_Task1_3_2 {
 //            }
 //            System.out.println("Наибольшее число: " + max);
 
-            int maximum = printMax(max);
+            double maximum = printMax(max);
     }
 
-    static int  findMax(int a, int b) {
-        int maximum;
+    static double  findMax(double a, double b) {
+        double maximum;
         if (a > b) {
             maximum = a;
         } else {
@@ -31,7 +31,7 @@ public class Draft2_Task1_3_2 {
         return maximum;
     }
 
-    static int printMax(int max) {
+    static double printMax(double max) {
         System.out.println("Наибольшее число: " + max);
         return max;
     }
